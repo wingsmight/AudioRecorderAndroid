ce2e3d489db6a16503d6c24840ff6c0f104e0651
wingsmight <wingsmight@gmail.com>
2022-01-17 23:00:24 +0700

    add theme mode switcher

--- app/src/main/java/com/wingsmight/audiorecorder/ui/settings/SettingsPreferencesFragment.java
+++ app/src/main/java/com/wingsmight/audiorecorder/ui/settings/SettingsPreferencesFragment.java
@@ -1,18 +1,80 @@
 package com.wingsmight.audiorecorder.ui.settings;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.BuildCompat;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.wingsmight.audiorecorder.MainActivity;
 import com.wingsmight.audiorecorder.R;
 
 public class SettingsPreferencesFragment extends PreferenceFragmentCompat {
 
-
+    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String newValue) {
+                Log.i("newValue", newValue);
+
+                if (newValue.equals(getResources().getString(R.string.themeModeListKey))) {
+                    ListPreference listPreference = findPreference(newValue);
+                    String currValue = listPreference.getValue();
+
+                    if (currValue.equals(getResources().getString(R.string.pf_light_on))) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else if (currValue.equals(getResources().getString(R.string.pf_light_off))) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        } else {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                        }
+                    }
+
+                    restartApp();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(preferenceChangeListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(preferenceChangeListener);
+    }
+
+
+    private void restartApp() {
+        Context context = getContext();
+        Intent mStartActivity = new Intent(context, MainActivity.class);
+        int mPendingIntentId = 123456;
+        PendingIntent mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId,    mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager mgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+        System.exit(0);
     }
 }
