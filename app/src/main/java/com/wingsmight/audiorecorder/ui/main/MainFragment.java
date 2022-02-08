caf851786f6848780ef739fd3e0541d9c08bc843
wingsmight <wingsmight@gmail.com>
2022-02-09 02:20:12 +0700

    add view for Main tab

--- app/src/main/java/com/wingsmight/audiorecorder/ui/main/MainFragment.java
+++ app/src/main/java/com/wingsmight/audiorecorder/ui/main/MainFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,29 +15,43 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.wingsmight.audiorecorder.MainActivity;
+import com.wingsmight.audiorecorder.R;
 import com.wingsmight.audiorecorder.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
-
-    private MainViewModel mainViewModel;
     private FragmentMainBinding binding;
+
+    private ImageButton switchRecordButton;
+    private View[] volumeRounds = new View[2];
+    private TextView timerTextView;
+
+    private boolean isRecording = false;
+
 
     @SuppressLint("RestrictedApi")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mainViewModel =
-                new ViewModelProvider(this).get(MainViewModel.class);
-
         binding = FragmentMainBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        mainViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        switchRecordButton = binding.switchRecordButton;
+        switchRecordButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                isRecording = !isRecording;
+
+                if (isRecording) {
+                    startRecording();
+                } else {
+                    stopRecording();
+                }
             }
         });
+
+        timerTextView = binding.timer;
+
+        volumeRounds[0] = binding.volumeRound0;
+        volumeRounds[1] = binding.volumeRound1;
 
         return root;
     }
@@ -46,5 +61,21 @@ public class MainFragment extends Fragment {
         super.onDestroyView();
 
         binding = null;
+    }
+
+    private void startRecording() {
+        switchRecordButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop_square));
+        timerTextView.setVisibility(View.VISIBLE);
+        setVolumeRoundsVisibility(View.VISIBLE);
+    }
+    private void stopRecording() {
+        switchRecordButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_play_arrow_24));
+        timerTextView.setVisibility(View.INVISIBLE);
+        setVolumeRoundsVisibility(View.INVISIBLE);
+    }
+    private void setVolumeRoundsVisibility(int visibility) {
+        for (View volumeRound : volumeRounds) {
+            volumeRound.setVisibility(visibility);
+        }
     }
 }
